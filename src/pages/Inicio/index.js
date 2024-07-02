16bd9dc2cb209cb9bb2ab1a1e5fc094c9d4694be
Edna2607 <eycardonaec@cesde.net>
2024-07-02 12:45:14 -0500

    Titulo del proyecto

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,13 +1,16 @@
 import Banner from "components/Banner";
 import Cabecera from "components/Cabecera/Cabecera";
 import Pie from "components/Pie";
+import Titulo from "components/Titulo";
 
 function Inicio() {
   return (
     <>
       <Cabecera></Cabecera>
       <Banner img="home" color="#154580" />
-
+      <Titulo>
+        <h1>Un lugar para guardar Tus videos Favoritos</h1>
+      </Titulo>
       <Pie />
     </>
   );
