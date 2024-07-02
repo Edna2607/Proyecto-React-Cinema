659acb0cdc08ad8b3b93c9fab015b03d35463c84
Edna2607 <eycardonaec@cesde.net>
2024-07-02 18:35:55 -0500

    estilos de las Cards y container.

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,15 +1,14 @@
-import Cabecera from "components/Cabecera/Cabecera";
 import Banner from "components/Banner";
 import Titulo from "components/Titulo";
 import Card from "components/Card";
-import styles from "./index.modules.css"
+import styles from "./index.module.css"
 import videos from "data/db.json"
-import Pie from "components/Pie";
+
 
 function Inicio() {
   return (
     <>
-      <Cabecera></Cabecera>
+      
       <Banner img="home" color="#154580" />
       <Titulo>
         <h1>Un lugar para guardar Tus videos Favoritos</h1>
@@ -21,7 +20,7 @@ function Inicio() {
         })}
       </section>
 
-      <Pie />
+     
     </>
   );
 }
