b3414a1e939ebb1b61bdad5948e5cadfea939af6
Edna2607 <eycardonaec@cesde.net>
2024-07-02 20:02:53 -0500

    Agregando los Favoritos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,8 +1,23 @@
+import Banner from "components/Banner";
 import styles from "./Favoritos.module.css";
+import Titulo from "components/Titulo";
+import Card from "components/Card";
+import { useFavoritosContext } from "context/favoritos";
 
-function Favoritos(){
-    return(
+function Favoritos() {
+    const { favorito } = useFavoritosContext();
+
+    return (
         <>
+            <Banner img="favorite" color="#AF7EAA" />
+            <Titulo>
+                <h1>Mis Favoritos</h1>
+            </Titulo>
+            <section className={styles.container}>
+                {favorito.map(fav => {
+                    return <Card {...fav} key={fav.id} />;
+                })}
+            </section>
         </>
     )
 }
