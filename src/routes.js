0dc88bee25f78a58eb3e83537507bc56bf05703a
Edna2607 <eycardonaec@cesde.net>
2024-07-03 19:10:40 -0500

    Pagina Base

--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,21 @@
-import Cabecera from "components/Cabecera/Cabecera";
-import Container from "components/Container";
-import Pie from "components/Pie";
 import Favoritos from "pages/Favoritos";
 import Inicio from "pages/Inicio";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import FavoritoProvider from "context/favoritos";
 import Player from "pages/Player";
 import NotFound from "pages/NotFound";
+import PaginaBase from "pages/PaginaBase";
 
 function AppRoutes() {
     return (
         <BrowserRouter>
-            <Cabecera />
-            <Container>
-                <FavoritoProvider>
-                    <Routes>
-                        <Route path="/" element={<Inicio />}></Route>
-                        <Route path="/favoritos" element={<Favoritos />}></Route>
-                        <Route path="/:id" element={<Player/>}></Route>
-                        <Route path="*" element={<NotFound/>}></Route>
-                    </Routes>
-                </FavoritoProvider>
-            </Container>
-
-            <Pie />
+            <Routes>
+                <Route path="/" element={<PaginaBase />}>
+                    <Route index element={<Inicio />}></Route>
+                    <Route path="favoritos" element={<Favoritos />}></Route>
+                    <Route path=":id" element={<Player />}></Route>
+                    <Route path="*" element={<NotFound />}></Route>
+                </Route>
+            </Routes>
         </BrowserRouter>
     );
 }
